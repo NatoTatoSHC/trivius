c5836ac03d2ff99bb864fa792135128f4d07437d
NatoTatoSHC <26lester@stagneselementary.org>
2025-04-22 19:14:10 -0500

    Add files via upload

---
+++ server.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var server = http.createServer(app);
+var { Server } = require('socket.io');
+var io = new Server(server);
+var os = require('os');
+
+var games = {
+    "000001":{
+        players: [
+            {
+                name:"name",
+                score:0
+            }
+        ],
+        isRunning: false
+    }
+};
+app.get('/', (req, res) => {
+    res.sendFile(__dirname+"/main.html");
+});
+io.on('connection', (socket) => {
+    socket.on('reqCodeVer', (code) => {
+        console.log(code in Object.keys(games));
+    });
+});
+
+server.listen(3000, () => {
+    console.log("Listening on port 3000");
+});
